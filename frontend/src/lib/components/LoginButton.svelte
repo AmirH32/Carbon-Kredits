92bc051551d0ea56b8127bb6bcf3ce97f9ada4cb
Joe Whitehead <34864012+M-Moon@users.noreply.github.com>
2024-10-17 23:56:40 +0100

    add env example

---
+++ frontend/src/lib/components/LoginButton.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+    import { contractAddress } from "$lib/stores/contractAddress";
+    import { keyId } from "$lib/stores/keyId";
+    import {
+        account,
+        send,
+        fundContract,
+        getContractId,
+    } from "$lib/passkeyClient";
+    import { getToastStore } from "@skeletonlabs/skeleton";
+    let toastStore = getToastStore();
+
+    async function signup() {
+        console.log("signing up");
+        try {
+            const {
+                keyId_base64: kid,
+                contractId: cid,
+                built,
+            } = await account.createWallet(
+                "Meridian Roller",
+                "High Stakes Roller",
+            );
+
+            keyId.set(kid);
+            console.log("key id", $keyId);
+            contractAddress.set(cid);
+            console.log("contract address", $contractAddress);
+
+            await send(built);
+            await fundContract($contractAddress);
+        } catch (err) {
+            console.log("err", err);
+            toastStore.trigger({
+                message:
+                    "Something went wrong signing up. Please try again later.",
+                background: "variant-filled-error",
+            });
+        }
+    }
+
+    async function login() {
+        console.log("logging in");
+        try {
+            const { keyId_base64: kid, contractId: cid } =
+                await account.connectWallet({
+                    getContractId,
+                });
+
+            keyId.set(kid);
+            console.log("key id", $keyId);
+            contractAddress.set(cid);
+            console.log("contract address", $contractAddress);
+        } catch (err) {
+            console.log("err", err);
+            toastStore.trigger({
+                message:
+                    "Something went wrong logging in. Please try again later.",
+                background: "variant-filled-error",
+            });
+        }
+    }
+
+    function logout() {
+        console.log("logging out");
+        try {
+            keyId.reset();
+            contractAddress.set("");
+            localStorage.removeItem("dg:keyId");
+            window.location.reload();
+        } catch (err) {
+            console.log("err", err);
+            toastStore.trigger({
+                message:
+                    "Something went wrong logging out. Please try again later.",
+                background: "variant-filled-error",
+            });
+        }
+    }
+</script>
+
+{#if $contractAddress}
+    <button type="button" class="btn variant-filled-secondary" on:click={logout}
+        >Logout</button
+    >
+{:else}
+    <button type="button" class="btn variant-filled-primary" on:click={signup}
+        >Signup</button
+    >
+    <button
+        type="button"
+        class="btn variant-filled-secondary"
+        on:click={() => login()}>Login</button
+    >
+{/if}
--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,5 +1,1 @@
 // place files you want to import through the `$lib` alias in this folder.
-
-async function generateToken(tokenAmount: number) {
-    console.log('wow')
-}
